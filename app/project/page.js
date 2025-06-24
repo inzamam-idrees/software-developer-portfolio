894fd722f3b24a87c3204b02b04e7b48b6491918
Inzamam Idrees <inzamam.idrees@nxb.com.pk>
2025-06-24 19:19:52 +0500

    fixed build issue

--- app/project/page.js
+++ app/project/page.js
@@ -20,7 +20,7 @@ async function page() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
           projectsData.map((project, index) => (
-            <ProjectCard project={project} />
+            <ProjectCard project={project} key={index} />
           ))
         }
       </div>

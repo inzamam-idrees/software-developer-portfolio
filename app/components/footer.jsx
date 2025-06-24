556ee46464222dcfb13cb43b0e8f290d5e7995f2
Inzamam Idrees <inzamam.idrees@nxb.com.pk>
2025-06-24 19:17:08 +0500

    update links

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/inzamam-idrees/" className="text-[#16f2b3]">Inzamam Idrees</Link>
+            © Software Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/inzamam-idrees/" className="text-[#16f2b3]">Inzamam Idrees</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href="https://github.com/inzamam-idrees/software-developer-portfolio"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href="https://github.com/inzamam-idrees/software-developer-portfolio/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
